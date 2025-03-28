776afa675c48c732458909d716d42e1e048f5dde
Krishna <krishnakumar_raut@epam.com>
2025-03-29 00:51:48 +0530

    fix the gateway-mvc bug

--- task-submission-service/src/main/java/com/krishna/controller/HomeController.java
+++ task-submission-service/src/main/java/com/krishna/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("submission")
     public ResponseEntity<String> homeController(){
         return ResponseEntity.ok("Welcome to Task Submission Service");
     }
